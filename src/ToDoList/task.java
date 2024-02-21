7f99c986a8946fcca1cfffadebca523b1cdfcb71
TheMarksan <marcosmelodossantos.com@gmail.com>
2024-02-21 09:37:09 -0300

    classes

---
+++ src/ToDoList/task.java
@@ -0,0 +1,14 @@
+package ToDoList;
+
+public class task {
+    String titulo;
+    String descricao;
+    public task(String titulo, String descricao){
+        this.titulo = titulo;
+        this.descricao = descricao;
+    }
+
+    public String showTitulo(){
+        return this.descricao;
+    }
+}
